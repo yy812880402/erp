4b021003b5e0ede67b7b1891541b0e7866fe72c9
zhangjiangfeng <jiangfeng.zhang@diveo.com.cn>
2019-07-15 09:33:25 +0800

    In Hello.java File Add Two Statem ent

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("hello Git);
+		System.out.println("认真上课是个好孩子！！！");
+		System.out.println("不认真上课不是好孩子！！！");
 	}
 }
